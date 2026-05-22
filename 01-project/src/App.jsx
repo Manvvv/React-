70ff59076bd4015b5318e822db76c7bd10b88c66
Sagar Choudhary <choudharysagar937@gmail.com>
2026-05-22 12:57:38 +0530

    01-project done

---
+++ 01-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="GrandParent">
+      <h1>Hello World</h1>
+      <h2>Welcome to React</h2>
+    </div>
+    <div id="Parent">
+      <h2>This is a child div</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
